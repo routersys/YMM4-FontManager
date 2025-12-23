1bf0b14c7eb2c4380c8a2a3d2370401505b9e812
routersys <machikado.mazoku.2@gmail.com>
2025-12-23 12:37:28 +0900

    UIの最適化

---
+++ FontManager/Converters/WidthToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FontManager.Converters
+{
+    public class WidthToVisibilityConverter : IValueConverter
+    {
+        public double Threshold { get; set; } = 850;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                return width < Threshold ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
